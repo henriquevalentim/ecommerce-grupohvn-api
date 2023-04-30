3e5ff7c87cf9d2541e5de31948953ecf4b60c859
Henrique Valentim Nunes <henriquevalentim16@gmail.com>
2023-04-30 13:12:59 -0300

    test: coment code test

--- src/main/controllers/order.js
+++ src/main/controllers/order.js
@@ -1,12 +1,12 @@
 const ProductRepository = require('../../infra/repository/productRepository')
 const AddressRepository = require('../../infra/repository/addressRepository')
 const OrderRepository = require('../../infra/repository/orderRepository')
-const UserRepository = require('../../infra/repository/userRepository')
+// const UserRepository = require('../../infra/repository/userRepository')
 const GetOrdersByUser = require('../../domain/usecases/order/getOrdersByUser')
 const GetFrete = require('../../domain/usecases/frete/getFrete')
 const PayCreditCard = require('../../domain/usecases/payment/payCreditCard')
 const ProcessOrder = require('../../domain/usecases/order/processOrder')
-const GetOrders = require('../../domain/usecases/order/getOrders')
+// const GetOrders = require('../../domain/usecases/order/getOrders')
 
 class OrderController {
   async getOrdersByUser(req, res) {
@@ -28,18 +28,18 @@ class OrderController {
     }
   }
 
-  async getOrders(req, res) {
-    try {
-      const getOrders = new GetOrders(OrderRepository, UserRepository)
+  // async getOrders(req, res) {
+  //   try {
+  //     const getOrders = new GetOrders(OrderRepository, UserRepository)
 
-      const response = await getOrders.execute()
+  //     const response = await getOrders.execute()
 
-      return res.status(200).json(response)
-    } catch (error) {
-      console.log(error)
-      return res.status(400).json({ message: error.message })
-    }
-  }
+  //     return res.status(200).json(response)
+  //   } catch (error) {
+  //     console.log(error)
+  //     return res.status(400).json({ message: error.message })
+  //   }
+  // }
 
   async processOrder(req, res) {
     try {
